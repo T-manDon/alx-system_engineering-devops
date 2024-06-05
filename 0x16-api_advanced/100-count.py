4f6ee312107764f8791ab328a52a975e6c911ec1
T-manDon <opiyoadiema@gmail.com>
2024-06-05 02:26:29 -0700

    all

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,9 +7,9 @@ import requests
 
 def count_words(subreddit, word_list, after=None, sort=True):
     """
-    Queries the Reddit API, parses the title of all hot articles,
-    and prints a sorted count of given keywords (case-insensitive, delimited
-    by spaces. Javascript should count as javascript, but java should not)
+    Query Reddit API and parse the title of all the hot articles,
+    and print the selected count for keywords (case-insensitive, delimited
+    by spaces. Javascript is considered javascript, but java should not)
     Ags:
         subreddit (str): name of subreddit
         word_list (list): keywords to look out for
